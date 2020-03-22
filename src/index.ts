14ea9b626fa0e367de45bf4aad6c59122fce31e6
Cam Smith <CSmith14@protonmail.com>
2020-03-22 00:21:29 -0600

    content

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import {readFileSync} from 'fs'
+import {isAbsolute, resolve} from 'path'
+
+interface EnvVars {
+  [key: string]: string;
+}
+
+function mapToObject( map: Map<string, string> ) {
+	const arr = [ ...map.entries() ]
+	return arr.reduce( ( carrier: any, entry ) => {
+		const [ k, v ] = entry
+		carrier[k] = v
+		return carrier
+	}, {} )
+}
+
+function mapVarsFromLines( lines: string[] ) {
+	const map = new Map()
+	lines
+		.filter( ( v ) => v.trim().length && v[0] !== '#' ) // Remove blank & commented
+		.forEach( ( line, index ) => {
+			const [ key, value ] = line.split( '=' ).map( ( s ) => s.trim() )
+			/* No key, No value, Key OR Value contain a space */
+			if ( !key || !value || [ key, value ].some( ( i ) => /\s/.test( i ) ) ) {
+				throw new Error( `FORMAT:${index + 1}` )
+			}
+			map.set( key, value )
+		} )
+	return map
+}
+
+/**
+ * Given a path to a file in .env format, returns key/value pairs in an object.
+ * @param {string} path The file path to read
+ * @returns {object} An object of corresponding key/value pairs
+ * @throws Error if file is not in valid format
+ */
+export function readSingle( path: string ): EnvVars {
+	const file = isAbsolute( path ) ? path : resolve( process.cwd(), path )
+	const content = readFileSync( file, 'utf8' )
+	const lines = content.split( '\n' )
+	try {
+		const map = mapVarsFromLines( lines )
+		return mapToObject( map )
+	} catch ( error ) {
+		const message = error.message || ''
+		if ( message.includes( 'FORMAT' ) ) {
+			throw new Error( `Invalid file format: ${path}:${parseInt( message.split( ':' )[1], 10 )}` )
+		}
+		throw error
+	}
+}
+
+/**
+ * @description Given an array of paths, will read each and add the key/value
+ * pairs to an object which is returned at the end. Any re-used keys will be overwritten by
+ * the most recent value.
+ * @param {string[]} paths An array of file paths to read.
+ * @returns {object} An aggregate object of key/value pairs
+ */
+export function readMultiple( paths: string[] ): EnvVars {
+	return paths.reduce( ( carrier: any, path ) => {
+		const obj = readSingle( path )
+		return {
+			...carrier,
+			...obj,
+		}
+	}, {} )
+}
---
+++ test/index.test.ts
@@ -0,0 +1,91 @@
+import {resolve, join} from 'path'
+import {expect} from 'chai'
+import {fancy} from 'fancy-test'
+import {readMultiple, readSingle} from '../src'
+
+/* test input files directory path */
+const inputDir = resolve( __dirname, 'input' )
+
+/* test input files and expected values */
+const envFiles = {
+	env1: {
+		path:  join( inputDir, '.env1' ),
+		value: {
+			Key1: 'Value1',
+			Key2: 'Value2',
+			Key3: '123456',
+		},
+	},
+	env2: {
+		path:  join( inputDir, '.env2' ),
+		value: {
+			KeyA: 'ValueA',
+			KeyB: 'ValueB',
+			KeyC: '123456',
+		},
+	},
+	env3: {
+		path:  join( inputDir, '.env3' ),
+		value: {
+			Key3: 'Value3',
+			KeyC: 'ValueC',
+		},
+	},
+	commented: {
+		path:  join( inputDir, '.commented' ),
+		value: {Key: 'Value'},
+	},
+	invalid1: {
+		path:  join( inputDir, '.invalid1' ),
+		value: /Invalid file format:/,
+	},
+	invalid2: {
+		path:  join( inputDir, '.invalid2' ),
+		value: /Invalid file format:/,
+	},
+}
+
+/* read-all paths and expected value */
+const multiple = {
+	paths: [
+		join( inputDir, '.env1' ),
+		join( inputDir, '.env2' ),
+		join( inputDir, '.env3' ),
+		join( inputDir, '.commented' ),
+	],
+	value: {
+		Key:  'Value',
+		Key1: 'Value1',
+		Key2: 'Value2',
+		Key3: 'Value3',
+		KeyA: 'ValueA',
+		KeyB: 'ValueB',
+		KeyC: 'ValueC',
+	}}
+
+describe( 'read-file', () => {
+	it( 'Reads key/value pairs', () => {
+		const envObject = readSingle( envFiles.env1.path )
+		expect( envObject ).to.deep.equal( envFiles.env1.value )
+	} )
+
+	it( 'Handles commented & empty lines', () => {
+		const envObject = readSingle( envFiles.commented.path )
+		expect( envObject ).to.deep.equal( envFiles.commented.value )
+	} )
+
+	fancy
+		.do( () => readSingle( envFiles.invalid1.path ) )
+		.catch( envFiles.invalid1.value )
+		.it( 'Throws on invalid format (missing key or value)' )
+
+	fancy
+		.do( () => readSingle( envFiles.invalid2.path ) )
+		.catch( envFiles.invalid2.value )
+		.it( 'Throws on invalid format (space in key or value)' )
+
+	it( 'Merges the results of multiple input files', () => {
+		const merged = readMultiple( multiple.paths )
+		expect( merged ).to.deep.equal( multiple.value )
+	} )
+} )
